9c8e65029c928c5acb36edfe850d28334bdde937
Erin DeVine <erinj291@gmail.com>
2024-06-30 16:11:38 -0400

    pushing up code for circle shape render

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 
 inquirer.prompt ([
     {
         type: 'input',
         name: 'text',
-        message: 'Please enter text (Must be 3 characters only)'
+        message: 'Please enter lgoo text (Must be up to 3 characters only)'
     },
 
     {
         type: 'input',
         name: 'color',
-        message: 'Please enter a color key word or hexadecimal number'
+        message: 'Please enter a color (key word or hexadecimal number) for the text'
     },
 
     {
         type: 'list',
         name: 'shape',
         message: 'Please select one of the following shapes',
-        choices: ['circle', 'triangle', 'square']  
-    }
+        choices: ['circle', 'square', 'triangle']  
+    },
 
-
+    {
+        type: 'input',
+        name: 'color',
+        message: 'Please enter a color (key word or hexadecimal number) for the shape'
+    },
 
 
 ])
+
+
+const generateSVG = function () {
+
+};
